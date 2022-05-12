edf15e56891792a7b7b2512f2b3c31abadec3d4b
KarolBrz <61661483+Karol1302@users.noreply.github.com>
2022-05-12 20:36:44 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Snake
+{
+    partial class Snake
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceController1 = new System.ServiceProcess.ServiceController();
+            this.scorelbl = new System.Windows.Forms.Label();
+            this.food = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // scorelbl
+            // 
+            this.scorelbl.AutoSize = true;
+            this.scorelbl.Location = new System.Drawing.Point(12, 28);
+            this.scorelbl.Name = "scorelbl";
+            this.scorelbl.Size = new System.Drawing.Size(87, 21);
+            this.scorelbl.TabIndex = 0;
+            this.scorelbl.Text = "Score:0";
+            // 
+            // food
+            // 
+            this.food.BackColor = System.Drawing.Color.Red;
+            this.food.Location = new System.Drawing.Point(289, 152);
+            this.food.Name = "food";
+            this.food.Size = new System.Drawing.Size(20, 21);
+            this.food.TabIndex = 1;
+            // 
+            // Snake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 500);
+            this.Controls.Add(this.food);
+            this.Controls.Add(this.scorelbl);
+            this.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Snake";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Snake_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceController serviceController1;
+        private System.Windows.Forms.Label scorelbl;
+        private System.Windows.Forms.Label food;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Snake : Form
+    {
+        int cols = 50, rows = 25, score = 0, dx = 0, dy = 0, front = 0, back = 0;
+
+        Piece [] snake = new Piece[1250];
+        List<int> avaiable = new List<int>();
+        bool[,] visit;
+        Random R =  new Random();
+        Timer T = new Timer();  
+
+        public Snake()
+        {
+
+            InitializeComponent();
+            intial();
+            launchTimer();
+        }
+
+        private void launchTimer()
+        {
+            T.Interval = 50;
+            T.Tick += move;
+        }
+
+        private void Snake_KeyDown(object sender, KeyEventArgs e)
+        {
+            dx = dy = 0;
+            switch(e.KeyCode)
+            {
+                case Keys.Right:
+                    dx = 20;
+                    break;
+                case Keys.Up:
+                    dy = -20;
+                    break;
+                case Keys.Left:
+                    dx = -20;
+                    break;
+                case Keys.Down:
+                    dy = 20;
+                    break;
+
+            }
+        }
+
+        private void move(object sender, EventArgs e)
+        {
+            int x = snake[front].Location.X, y = snake[front].Location.Y;
+            if (dx == 0 && dy == 0) return;
+            if(gameover(x + dx, y + dy))
+            {
+                T.Stop();
+                MessageBox.Show("Game Over!");
+                return;
+            }
+            if(collisionfood(x + dx, y + dy))
+            {
+                score += 1;
+                scorelbl.Text = "Score: " + score.ToString();
+                if (hits((y + dy) / 20, (x + dx) / 20)) return;
+                Piece head = new Piece(x + dx, y + dy);
+                front = (front - 1 + 1250) / 1250;
+                snake[front] = head;
+                visit[head.Location.Y / 20, head.Location.X / 20] = true;
+                Controls.Add(head);
+                RandomFood();
+            }
+            else
+            {
+                if (hits((y + dy) / 20, (x + dx) / 20)) return;
+                visit[snake[back].Location.Y / 20, Location.X / 20] = false;
+                front = (front - 1 + 1250) % 1250;
+                snake[front] = snake[back];
+                snake[front].Location = new Point(x + dx, y + dy);
+                back = (back - 1 + 1250) % 1250;
+                visit[(y + dy) / 20, (x - dx) / 20] = true;
+            }
+        }
+
+        private void RandomFood()
+        {
+            avaiable.Clear();
+            for(int i = 0; i < rows; i++)
+                for(int j = 0; j < cols; j++)
+                    if(!visit[i,j])avaiable.Add(i + cols + j);
+            int idx = R.Next(avaiable.Count) % avaiable.Count;
+            food.Left = (avaiable[idx] * 20) % Width;
+            food.Top = (avaiable[idx] * 20) % Width * 20;
+
+        }
+
+        private bool hits(int x, int y)
+        {
+            if(visit[x, y])
+            {
+                T.Stop();
+                MessageBox.Show("Snake hits his body!");
+                return true;
+            }
+            return false;
+            
+        }
+
+        private bool collisionfood(int x, int y)
+        {
+            return x == food.Location.X && y == food.Location.Y;
+        }
+
+        private bool gameover(int x, int y)
+        {
+            return x < 0 || y < 0 || x > 980 || y > 480;
+        }
+
+        private void intial()
+        {
+            visit = new bool [rows, cols];
+            Piece head = new Piece((R.Next() % cols) * 20,(R.Next() % rows) * 20 );
+            food.Location = new Point((R.Next() % cols) * 20, (R.Next() % rows) * 20);
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j < cols; j++)
+                {
+                    visit[i,j] = false;
+                    avaiable.Add(i * cols +j);
+
+                }
+            }
+            visit[head.Location.Y / 20, head.Location.X / 20] = true;
+            avaiable.Remove(head.Location.Y / 20 * cols + head.Location.X / 20);  
+            Controls.Add(head); snake[front] = head;
+
+
+        }
+    }
+}
